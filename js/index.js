ef6a2eb725cb91c5eb1cb61350c45909428e132d
CaptinBata <nbata1997@gmail.com>
2020-07-11 16:32:45 +0100

    Added loading of canvas

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
-function main()
-{
+class Game {
+    constructor() {
+        this.SetupCanvas();
+    }
 
+    SetupCanvas() {
+        this.canvas = document.getElementById("gameCanvas");
+        this.canvas.width = this.GetWindowWidth();
+        this.canvas.height = this.GetWindowHeight();
+        this.context = this.canvas.getContext("2d");
+    }
+
+    GetWindowWidth() {
+        return window.innerWidth - 25;
+    };
+    GetWindowHeight() {
+        return window.innerHeight - 25;
+    };
+
+    main() {
+
+    }
 }
 
-main();
+let game = new Game()
+game.main();
