778b64f891fad3f83456ae43f8bfabb210115626
CaptinBata <nbata1997@gmail.com>
2020-07-11 16:34:46 +0100

    Added a black background to context

--- js/index.js
+++ js/index.js
@@ -8,7 +8,14 @@ class Game {
         this.canvas.width = this.GetWindowWidth();
         this.canvas.height = this.GetWindowHeight();
         this.context = this.canvas.getContext("2d");
+        this.ClearScreen();
     }
+
+    ClearScreen() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = "#000000";
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    };
 
     GetWindowWidth() {
         return window.innerWidth - 25;
