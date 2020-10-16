ebac281835febfa072008b8c6f395b28204f2619
CaptinBata <nbata1997@gmail.com>
2020-10-16 23:00:53 +0100

    More aliens

--- Nyk/js/alien.js
+++ Nyk/js/alien.js
@@ -5,6 +5,16 @@ class Alien extends GameObject {
         this.setDrawObject({
             "main": {
                 "drawPoints": [
+                    new Vector(-10, -25),
+                    new Vector(30, 0),
+                    new Vector(30, -30),
+                    new Vector(2.5, -30),
+                    new Vector(2.5, -40),
+                    new Vector(-2.5, -40),
+                    new Vector(-2.5, -30),
+                    new Vector(-30, -30),
+                    new Vector(-30, 0),
+                    new Vector(-2.5, 0),
                     new Vector(2.5, 0),
                     new Vector(30, 0),
                     new Vector(30, -30),
@@ -15,9 +25,29 @@ class Alien extends GameObject {
                     new Vector(-30, -30),
                     new Vector(-30, 0),
                     new Vector(-2.5, 0),
+                    new Vector(2.5, 0),
+                    new Vector(30, 0),
+                    new Vector(30, -30),
+                    new Vector(2.5, -30),
+                    new Vector(2.5, -40),
+                    new Vector(-2.5, -40),
+                    new Vector(-2.5, -30),
+                    new Vector(-30, -30),
+                    new Vector(-30, 0),
+                    new Vector(-2.5, 0),
+                    new Vector(2.5, 0),
+                    new Vector(30, 0),
+                    new Vector(30, -30),
+                    new Vector(2.5, -30),
+                    new Vector(2.5, -40),
+                    new Vector(-2.5, -40),
+                    new Vector(-2.5, -30),
                 ],
                 "fillColour": "#ffffff",
-            }
+            },
+            "left eye": {},
+            "right eye": {},
+            "mouth": {}
         })
 
     }
