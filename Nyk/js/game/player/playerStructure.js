5371d02170d551804127bfcf8ed3330c0b65ebc4
CaptinBata <nbata1997@gmail.com>
2021-02-11 12:47:21 +0000

    Added missing new file

---
+++ Nyk/js/game/player/playerStructure.js
@@ -0,0 +1,15 @@
+player = {
+    "drawPoints": [
+        new Vector(-30, 15),
+        new Vector(-30, -15),
+        new Vector(-2.5, -15),
+        new Vector(-2.5, -25),
+        new Vector(2.5, -25),
+        new Vector(2.5, -15),
+        new Vector(30, -15),
+        new Vector(30, 15),
+        new Vector(-30, 15),
+    ],
+    "fillColour": "#ffffff",
+    "strokeColour": "#ffffff",
+}
